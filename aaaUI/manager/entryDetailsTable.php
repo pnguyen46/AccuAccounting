1dfc0ddeb9c5143c7cf68e4165ddf6d2628aa1c6
TronGuyen <pnguye894@gmail.com>
2018-12-03 15:44:33 -0500

    sss

--- aaaUI/manager/entryDetailsTable.php
+++ aaaUI/manager/entryDetailsTable.php
@@ -1,6 +1,6 @@
 <?php
 
- $link = mysqli_connect("den1.mysql1.gear.host", "accudb", "Fi9A-342?v5W", "aaadb");
+ $link = mysqli_connect("den1.mysql1.gear.host", "accudb", "Fi9A-342?v5W", "accudb");
 if($link === false){die("ERROR: Could not connect. " . mysqli_connect_error());}
 
 
