c0b32ff17d2cb25999eeda9327d4da2098c1a426
FrankDaTankB <36753033+FrankDaTankB@users.noreply.github.com>
2018-12-05 02:03:23 -0500

    cancel button added welcome page added

---
+++ aaaUI/auth/welcome.php
@@ -0,0 +1,32 @@
+<html lang="en">
+    <head>
+        <title>
+           AccuAccount
+        </title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+        <script src="https://code.jquery.com/jquery-1.12.4.js">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.5/validator.min.js">
+        </script>
+    </head>
+    <body>
+        <br>
+        <div class="container" align="center">
+            <div class="panel panel-primary" style="width:400px;margin:0px auto">
+			<img src="../../images/logo.png" style="margin-top: 0px !important; margin-left: -0px !important; width:130px;height:100px;">
+              <div class="panel-heading">Welcome to AccuAccount!</div>
+              <div class="panel-body">
+                  <div class="form-group">
+					  <button onclick="window.location.href='login.php'" class="btn btn-primary" name="login"type="submit">Login</button>
+                  </div>
+				  <div class="form-group">
+				     <button onclick="window.location.href='registerUser.php'" type="button" class="btn btn-outline btn-success">Register</button>
+				  </div>
+              </div>
+            </div>
+        </div>
+    </body>
+</html>
