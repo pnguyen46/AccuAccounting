7f735c9b1db9e49ed56285069531fe6ddf2ab126
FrankDaTankB <36753033+FrankDaTankB@users.noreply.github.com>
2018-12-05 13:13:26 -0500

    table log

--- aaaUI/static/logs.php
+++ aaaUI/static/logs.php
@@ -30,6 +30,9 @@ include 'base.php';
 	<!-- Custom Fonts -->
 	<link href="../../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
+	<script type="text/javascript" src="../../js/sb-admin-2.js"></script>
+
+
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
