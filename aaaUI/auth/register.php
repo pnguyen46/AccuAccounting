08f09a5c4843ce76430f6b38b60a7bca8d2cd2e0
FrankDaTankB <36753033+FrankDaTankB@users.noreply.github.com>
2018-12-05 12:25:25 -0500

    registration success msg

--- aaaUI/auth/register.php
+++ aaaUI/auth/register.php
@@ -19,8 +19,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
       VALUES ('$Email','$FirstName','$LastName','$password')";
 
       if ($mysqli->query($sql) == true) {
-        $_SESSION['message'] = "Registration successful! Account has been added, thank you!";
-        header("Location: login.php");
+        header("Location: regsuccess.php");
         exit();
       }
       else {
